502393abbe095040120a3c2c2a3ff935ef25bdcf
Gabriela <gabrielaaortiz8@gmail.com>
2024-06-13 11:40:53 -0700

    fixed logout route and button

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -45,7 +45,7 @@ router.post('/login', async (req, res) => {
 
 // User logout
 router.post("/logout", (req, res) => {
-    if (req.session.logged_in) {
+    if (req.session.loggedIn) {
         req.session.destroy(() => {
             res.status(204).end();
         });
